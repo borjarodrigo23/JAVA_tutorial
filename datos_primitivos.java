f46b520e5d44817608fae1426beaaf2cf5de0307
Borja J. Rodrigo <98977870+borjarodrigo23@users.noreply.github.com>
2022-02-04 11:10:25 +0100

    Add files via upload

---
+++ datos_primitivos.java
@@ -0,0 +1,26 @@
+package JAVA;
+
+public class datos_primitivos {
+    public static void main (String[] args) {
+        System.out.println("------------------"); //Separador de los directorios del terminal
+        
+        //Dato String para escribir carácteres:
+        String name = "Alumno"; //Declaramos el String name llamado "Alumno"
+        System.out.println("Mi nombre es " +name + ".");
+        
+        //Dato Enteras para escribir números
+        int edad = 23;
+        System.out.println("Tengo " +edad + "años.");
+
+        //Dato Double para escribir números decimales:
+        double altura = 1.78;
+        System.out.println("Mido " +altura + "metros.");
+
+        //Dato Boolean para evaluar si una variable es True or False.
+        boolean soy_alto = altura > 180 && edad < 22;
+        System.out.println("¿Me puedo considerar alto? La respuesta es: " +soy_alto + ".");
+       
+        //Si queremos expresarlo todo en una misma línea basta con cambiar System.out.println("") por System.out.print("")
+        System.out.println("------------------"); //Separador de los directorios del terminal
+    }
+}
---
+++ hello_world.java
@@ -0,0 +1,12 @@
+package JAVA;
+
+public class hello_world { //El término class debe ser el mismo que el nombre del arichvo
+    public static void main (String[] args) { //Cabecera inicial para empezar un programa
+        System.out.println("------------------"); 
+
+        System.out.println("Hello World!"); //Instrucción para imprimir en la terminal una instrucción, en este caso un String ("palabra")
+        
+        System.out.println("------------------"); 
+    }    
+}
+
